b90a8c796690b140fdbabf958dbcd2f750d220b4
Tan Yi Hao <tanericash@gmail.com>
2016-09-17 18:12:15 +0800

    integrated keypad

--- Source/Source.ino
+++ Source/Source.ino
@@ -1,6 +1,6 @@
-//subsystems: 
+//subsystems:
 //1. DONE bare arduino able to generate accurate Totps with secret key and time
-//2. integration of keypad and typing, "enter"
+//2. DONE integration of keypad and typing, "enter"
 //3. DONE integration of lcd screen and print outputs/status
 //4. DONE integration of hardware real time clock so that arduino can sync time even when powered off
 //5. integration of keypad inputs and comparison with the correct otp
@@ -16,6 +16,8 @@
 #include "Wire.h"
 #include "LiquidCrystal_I2C.h"
 
+#include "Keypad.h"
+
 // The shared secret is MyLegoDoor// SecretKey1
 uint8_t hmacKey[] = {0x53, 0x65, 0x63, 0x72, 0x65, 0x74, 0x4b, 0x65, 0x79, 0x31}; //{0x4d, 0x79, 0x4c, 0x65, 0x67, 0x6f, 0x44, 0x6f, 0x6f, 0x72};
 
@@ -24,31 +26,60 @@ TOTP totp = TOTP(hmacKey, 10);
 char code[7];
 
 //RTC Set pins:  CE, IO,CLK
-DS1302RTC RTC(5, 6, 7);
+DS1302RTC RTC(10, 11, 12);
 
 //I2C LCD
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 
+//keypad
+const byte numRows = 4; //number of rows on the keypad
+const byte numCols = 4; //number of columns on the keypad
+//keymap defines the key pressed according to the row and columns just as appears on the keypad
+char keymap[numRows][numCols] =
+{
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+//Code that shows the the keypad connections to the arduino terminals
+byte rowPins[numRows] = {9, 8, 7, 6}; //Rows 0 to 3
+byte colPins[numCols] = {5, 4, 3, 2}; //Columns 0 to 3
+//initializes an instance of the Keypad class
+Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+
+//variables
+
+
 
 void setup() {
-  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH); //vcc for hw rtc
 
-  
   Serial.begin(9600);
 
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
 
-//  rtc.stopRTC();
-//  
-//  // Adjust the following values to match the current date and time
-//  // and power on Arduino at the time set (use GMT timezone!)
-//  rtc.setDate(17, 9, 2016); //dd m yyyy
-//  rtc.setTime(04, 44, 30); //hh mm ss , gmt time
-//  
-//  rtc.startRTC();
+  //  rtc.stopRTC();
+  //
+  //  // Adjust the following values to match the current date and time
+  //  // and power on Arduino at the time set (use GMT timezone!)
+  //  rtc.setDate(17, 9, 2016); //dd m yyyy
+  //  rtc.setTime(04, 44, 30); //hh mm ss , gmt time
+  //
+  //  rtc.startRTC();
 }
 
 void loop() {
+
+  //keypad stuff
+  char keypressed = myKeypad.getKey();
+  if (keypressed != NO_KEY)
+  {
+    Serial.print(keypressed);
+  }
+
+  lcd.setCursor(0,1); //to print the key that is currently typed in
+  lcd.print("");
 
   //long GMT = rtc.getTimestamp(); //this is for the softRTC internal to arduino
 
@@ -56,7 +87,7 @@ void loop() {
   //Serial.print(RTC.get());
   long GMT = RTC.get(); //this is to get the time from the hardware RTC
   char* newCode = totp.getCode(GMT);
-  if(strcmp(code, newCode) != 0) {
+  if (strcmp(code, newCode) != 0) {
     strcpy(code, newCode);
     //Serial.println(rtc.getTimestamp()); //to print soft time
     //Serial.println(GMT);
@@ -65,32 +96,32 @@ void loop() {
 
     tmElements_t tm;
     if (! RTC.read(tm)) {
-    Serial.println("Valid for:"); //prints the time
-    Serial.print("  Time = ");
-    print2digits(tm.Hour);
-    Serial.write(':');
-    print2digits(tm.Minute);
-    Serial.write(':');
-    print2digits(tm.Second);
-    Serial.print(", Date (D/M/Y) = ");
-    Serial.print(tm.Day);
-    Serial.write('/');
-    Serial.print(tm.Month);
-    Serial.write('/');
-    Serial.print(tmYearToCalendar(tm.Year));
-    Serial.print(", DoW = ");
-    Serial.print(tm.Wday);
-    Serial.println();
+      Serial.println("Valid for:"); //prints the time
+      Serial.print("  Time = ");
+      print2digits(tm.Hour);
+      Serial.write(':');
+      print2digits(tm.Minute);
+      Serial.write(':');
+      print2digits(tm.Second);
+      Serial.print(", Date (D/M/Y) = ");
+      Serial.print(tm.Day);
+      Serial.write('/');
+      Serial.print(tm.Month);
+      Serial.write('/');
+      Serial.print(tmYearToCalendar(tm.Year));
+      Serial.print(", DoW = ");
+      Serial.print(tm.Wday);
+      Serial.println();
     }
 
     //lcd outputs
     lcd.clear();
-    lcd.setCursor(0,0); //Start at character 4 on line 0
+    lcd.setCursor(0, 0); //Start at character 4 on line 0
     lcd.print("OTP: ");
     lcd.print(code);
     //lcd.setCursor(0,1);
-    
-  }  
+
+  }
 }
 
 void print2digits(int number) {
